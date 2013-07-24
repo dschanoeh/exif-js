0dd08592c0ec6f52c6cf995bca6a9f7dfa438018
Jan-Niklas Meier <dschanoeh@googlemail.com>
2013-07-24 16:23:19 +0200

    Added GPS information

--- replaceImages.js
+++ replaceImages.js
@@ -1,46 +1,69 @@
 var replaceImage=function(im) {
     EXIF.getData(im, function() {
-        var make = EXIF.getTag(im, "Make"),
+        make = EXIF.getTag(im, "Make"),
         model = EXIF.getTag(im, "Model");
+        lata = EXIF.getTag(im, "GPSLatitude");
+        lona = EXIF.getTag(im, "GPSLongitude");
         aperture = parseFloat(EXIF.getTag(im, "FNumber"));
         exposure = parseFloat(EXIF.getTag(im, "ExposureTime"));
         iso = parseFloat(EXIF.getTag(im, "ISOSpeedRatings"));
         focalLength = parseFloat(EXIF.getTag(im, "FocalLength"));
         var text = "";
+        var overlaytext = "";
         var fields = 0;
 
         if(!isNaN(exposure)) {
-            if(exposure < 1)
+            if(exposure < 1) {
                 text = text + "1/" + Math.round(1.0/exposure);
-            else
+                overlaytext = overlaytext + "1/" + Math.round(1.0/exposure);
+            } else {
                 text = text + Math.round(exposure);
+                overlaytext = overlaytext + Math.round(exposure);
+            }
             fields++;
         }
 
         if(!isNaN(aperture)) {
             text = text + " at f/" + aperture;
+            overlaytext = overlaytext + " at f/" + aperture;
             fields++;
         }
 
         if(!isNaN(iso)) {
             text = text + " with ISO " + iso;
+            overlaytext = overlaytext + " with ISO " + iso;
             fields++;
         }
         if(make && model) {
             /* Make sure make is not displayed twice */
-            if(model.indexOf(make) === 0)
+            if(model.indexOf(make) === 0) {
                 text = text + " (" + model;
-            else
+                overlaytext = overlaytext + " (" + model;
+            } else {
                 text = text + " (" + make + " " + model;
+                overlaytext = overlaytext + " (" + make + " " + model;
+            }
             fields+=2;
         }
 
         if(!isNaN(focalLength)) {
             text = text + " at " + focalLength + "mm";
+            overlaytext = overlaytext + " at " + focalLength + "mm";
             fields++;
         }
 
         text += ")";
+        overlaytext += ")";
+
+        if(lata && lona) {
+            lat = lata[0] + (1.0/60.0) * lata[1];
+            lon = lona[0] + (1.0/60.0) * lona[1];
+            overlaytext += ' <a target="_blank" href="http://maps.google.com/?q=';
+            overlaytext += lat.toString();
+            overlaytext += ',';
+            overlaytext += lon.toString();
+            overlaytext += '">[GPS]</a>';
+        }
 
         /* Only display if there is information */
         if(fields > 3) {
@@ -58,7 +81,7 @@ var replaceImage=function(im) {
 
                 var inner = document.createElement('div');
                 inner.setAttribute("class", "exif");
-                inner.innerHTML = text;
+                inner.innerHTML = overlaytext;
 
                 outer.appendChild(im.parentNode);
                 outer.appendChild(inner);
